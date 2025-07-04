f56388daffa5feae152efc9d148c78f65b0166f9
Romanov, Evgeny <evgeny.romanov@intel.com>
2025-07-04 14:56:57 +0200

    Fied preprocessing is optional

--- geti_sdk/data_models/media.py
+++ geti_sdk/data_models/media.py
@@ -129,7 +129,7 @@ class MediaItem:
     type: str = attr.field(converter=str_to_media_type)
     upload_time: str = attr.field(converter=str_to_datetime)
     media_information: MediaInformation
-    preprocessing: MediaPreprocessing
+    preprocessing: Optional[MediaPreprocessing] = None # preprocessing was added in Geti 2.10
     annotation_state_per_task: Optional[List[TaskAnnotationState]] = None
     thumbnail: Optional[str] = None
     uploader_id: Optional[str] = None
